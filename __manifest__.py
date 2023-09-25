3780e2de66f860b7661c4466f41f7c8232415a75
rocketgithub <rodrigo@aquih.com>
2023-09-25 09:39:14 -0600

    Versión 16.0

--- __manifest__.py
+++ __manifest__.py
@@ -1,23 +1,17 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Oky",
-
     'summary': """Oky""",
-
     'description': """
         Módulo para Oky
     """,
-
     'author': "aquíH",
     'website': "http://www.aquih.com",
-
     'category': 'Uncategorized',
-    'version': '0.1',
-
-    'depends': ['base','account'],
-
+    'version': '1.0',
+    'depends': ['base', 'account'],
     'data': [
-        'views/account_view.xml',
+        #'views/account_view.xml',
         'views/res_partner_views.xml',
         'views/report.xml',
         'views/reporte_invoice1.xml',
